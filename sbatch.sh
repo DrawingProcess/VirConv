d0f2500fac0713355ed80287dd2c12de28b2270e
DrawingProcess <sjchoi.dp@gmail.com>
2024-04-21 19:38:49 +0900

    sbatch fine tunning

--- sbatch.sh
+++ sbatch.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/bash
 
-<<<<<<< HEAD
 #SBATCH -J VirConv-train-VirConv-S-only3d-Tepoch40
-=======
-#SBATCH -J VirConv-train-VirConv-T-only3d
->>>>>>> 9d64ea8def8235b8e5903fef02ab33f06f932818
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=32G
@@ -29,7 +25,6 @@ hostname
 # python setup.py develop
 
 cd tools
-<<<<<<< HEAD
 # python3 train.py --cfg_file cfgs/models/kitti/VirConv-L.yaml --extra_tag only2d
 # python3 train.py --cfg_file cfgs/models/kitti/VirConv-T.yaml --extra_tag only2d
 python3 train.py --cfg_file cfgs/models/kitti/VirConv-S.yaml --extra_tag only3d --epochs 10 --pretrained_model ../output/models/kitti/VirConv-T/only3d/ckpt/checkpoint_epoch_40.pth
@@ -44,12 +39,5 @@ python3 train.py --cfg_file cfgs/models/kitti/VirConv-S.yaml --extra_tag only3d 
 # python3 test.py --cfg_file cfgs/models/kitti/VirConv-T.yaml --extra_tag only2d --ckpt ../output/models/kitti/VirConv-T/only2d/ckpt/checkpoint_epoch_40.pth
 # python3 test.py --cfg_file cfgs/models/kitti/VirConv-T.yaml --extra_tag only3d --ckpt ../output/models/kitti/VirConv-T/only3d/ckpt/checkpoint_epoch_40.pth
 # python3 test.py --cfg_file cfgs/models/kitti/VirConv-S.yaml --extra_tag only2d --ckpt ../output/models/kitti/VirConv-S/only3d/ckpt/checkpoint_epoch_5.pth
-=======
-python3 train.py --cfg_file cfgs/models/kitti/VirConv-T.yaml --extra_tag only2d
-# python3 train.py --cfg_file cfgs/models/kitti/VirConv-T.yaml
-# python3 train.py --cfg_file cfgs/models/kitti/VirConv-S.yaml --pretrained_model ../output/models/kitti/VirConv-T/default/ckpt/checkpoint_epoch_40.pth
-
-# python3 test.py --cfg_file cfgs/models/kitti/VirConv-L.yaml --ckpt ../output/models/kitti/VirConv-L/default/ckpt/checkpoint_epoch_50.pth
->>>>>>> 9d64ea8def8235b8e5903fef02ab33f06f932818
 
 exit 0
